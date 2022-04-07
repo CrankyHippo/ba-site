cbd778d37c2d869b5c22d933dac29de70c300fd0
Bryan Burnett <crankyhippo@gmail.com>
2022-04-06 17:10:24 -0700

    update

---
+++ src/components/contentbox.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+export default function ContentBox() {
+  return (
+    <div className="fade">
+      <p className="content">
+        <h1>Title</h1>
+        <hr></hr>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Euismod elementum
+        nisi quis eleifend quam adipiscing. Mi proin sed libero enim sed
+        faucibus. Lorem ipsum dolor sit amet consectetur adipiscing elit. Non
+        arcu risus quis varius quam quisque id diam vel. Ut pharetra sit amet
+        aliquam id diam maecenas ultricies. Enim ut sem viverra aliquet eget sit
+        amet tellus cras. Suspendisse sed nisi lacus sed viverra tellus. Sed
+        elementum tempus egestas sed sed risus pretium quam vulputate. Vel quam
+        elementum pulvinar etiam non quam lacus suspendisse. Lorem sed risus
+        ultricies tristique nulla aliquet. Tincidunt lobortis feugiat vivamus
+        at. Consectetur lorem donec massa sapien faucibus et molestie ac.
+        Consequat id porta nibh venenatis. Et egestas quis ipsum suspendisse.
+        Neque laoreet suspendisse interdum consectetur libero id faucibus nisl.
+        Id faucibus nisl tincidunt eget nullam non nisi est sit. Malesuada fames
+        ac turpis egestas integer eget. Aenean et tortor at risus viverra
+        adipiscing at. Dolor morbi non arcu risus quis varius quam. Dignissim
+        sodales ut eu sem integer vitae justo eget magna.
+      </p>
+    </div>
+  );
+}
